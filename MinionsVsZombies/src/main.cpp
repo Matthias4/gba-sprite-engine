2a60dd8e209017091700ef5e6125c486093a0272
matthias <m@mdekeyser.be>
2019-11-15 15:32:34 +0100

    Added loop in main.cpp

--- MinionsVsZombies/src/main.cpp
+++ MinionsVsZombies/src/main.cpp
@@ -2,7 +2,16 @@
 // Created on 11/15/19.
 //
 
+#include <libgba-sprite-engine/gba_engine.h>
+
 int main() {
+
+    std::shared_ptr<GBAEngine> engine(new GBAEngine());
+
+    for (;;) {
+        engine->update();
+        engine->delay(1000);
+    }
 
     return 0;
 }
