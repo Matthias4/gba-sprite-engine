036651744955523bb5bfe8a0d420318adb312fbb
matthias <m@mdekeyser.be>
2019-11-25 14:38:50 +0100

    Added sub-project to MinionsVsZombies for unit tests Fixed bug in gba-sprite-engine unit tests' CMakeLists.txt that would cause build to fail on Linux

---
+++ MinionsVsZombies/test/TestFile.cpp
@@ -0,0 +1,4 @@
+//
+// Created by matthias on 11/25/19.
+//
+
---
+++ MinionsVsZombies/test/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by matthias on 11/25/19.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
