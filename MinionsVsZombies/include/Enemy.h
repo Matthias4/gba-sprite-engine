2c17559e35514bfe1b0bbb9fcace1e0c360a9cff
Anouk Michiels <anouk.michiels@student.uhasselt.be>
2019-11-26 19:14:40 +0100

    Added Enemy class.

---
+++ MinionsVsZombies/include/Enemy.h
@@ -0,0 +1,19 @@
+//
+// Created by michi on 26/11/2019.
+//
+
+#ifndef GBA_SPRITE_ENGINE_PROJECT_ENEMY_H
+#define GBA_SPRITE_ENGINE_PROJECT_ENEMY_H
+
+
+class Enemy {
+private:
+    int health;
+
+public:
+    int getHealth();
+
+};
+
+
+#endif //GBA_SPRITE_ENGINE_PROJECT_ENEMY_H
---
+++ MinionsVsZombies/src/Enemy.cpp
@@ -0,0 +1,9 @@
+//
+// Created by michi on 26/11/2019.
+//
+
+#include "Enemy.h"
+
+int Enemy::getHealth() {
+    return health;
+}
