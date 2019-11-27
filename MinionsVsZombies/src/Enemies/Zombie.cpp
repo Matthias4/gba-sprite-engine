b65482924def5816b1e76bd4cb7b039d1d81cf1c
matthias <m@mdekeyser.be>
2019-11-27 19:41:48 +0100

    Removed FIXME from Zombie::walk

--- MinionsVsZombies/src/Enemies/Zombie.cpp
+++ MinionsVsZombies/src/Enemies/Zombie.cpp
@@ -20,10 +20,12 @@ int Zombie::getPosition() {
     return position;
 }
 
-void Zombie::walk() {//FIXME: Volgens mij niet nodig: Level past de locatiearray aan (en geeft de sprite waarschijnlijk een nieuwe positie)
+void Zombie::walk() {
                         // Ging level ook de locatie van de zombies bijhouden? Ik dacht alleen van de minions. Het was mijn veronderstelling dat level gewoon een lijst van zombies heeft
                         // en dat de zombies zelf weten waar ze zijn en zelf vooruitwandelen enzo. Als we met een locatiearray voor de zombies zouden werken
                         //dan moeten ze hun eigen positie en row ook niet bijhouden wel?
+
+                        //Juist... Zombies zitten inderdaad niet in die array
     // wandel wandel
     //position--; ofzo
 }
