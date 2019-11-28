4f79f79221925ddd05cfc2af5ffc52d89d15a022
Luc Aarts <luc.aarts@student.uhasselt.be>
2019-11-28 11:58:24 +0100

    created the source files for the images

---
+++ MinionsVsZombies/img/png/minion.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(minion)
+
+//======================================================================
+//
+//	minion, 32x96@8, 
+//	+ 48 tiles not compressed
+//	Total size: 3072 = 3072
+//
+//	Time-stamp: 2019-11-28, 11:55:08
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_MINION_H
+#define GRIT_MINION_H
+
+#define minionTilesLen 3072
+extern const unsigned int minionTiles[768];
+
+#endif // GRIT_MINION_H
+
+//}}BLOCK(minion)
---
+++ MinionsVsZombies/img/png/zombie.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(zombie)
+
+//======================================================================
+//
+//	zombie, 32x256@8, 
+//	+ 128 tiles not compressed
+//	Total size: 8192 = 8192
+//
+//	Time-stamp: 2019-11-28, 11:55:08
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_ZOMBIE_H
+#define GRIT_ZOMBIE_H
+
+#define zombieTilesLen 8192
+extern const unsigned int zombieTiles[2048];
+
+#endif // GRIT_ZOMBIE_H
+
+//}}BLOCK(zombie)
