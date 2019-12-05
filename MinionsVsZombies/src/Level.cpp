4f1cafc4815c6de147e6ed9b1c614d983bbcdef0
Anouk Michiels <anouk.michiels@student.uhasselt.be>
2019-12-05 10:54:12 +0100

    Fixed crashing when going back to mainmenu. Gave minion epilepsy.

--- MinionsVsZombies/src/Level.cpp
+++ MinionsVsZombies/src/Level.cpp
@@ -127,11 +127,15 @@ void Level::load() {
 }
 
 void Level::tick(u16 keys) {
+
+    if (keys & KEY_A) {// A key (x on emulator) //
+        engine->setScene(new MainMenu(engine)); //Eventueel kunnen we hier een boodschap geven 'Are you sure you want to quit the level?' ofzo..
+        return;
+    }
+
     TextStream::instance().setText(std::string("Flowers: " + std::to_string(flowers)), 1, 1);
     TextStream::instance().setText(std::string("Wave: " + std::to_string(waveNumber + 1) + " / Zombies: " + std::to_string(zombies.size())), 3, 1);// @Anouk, meer testen Toppie :D
-    if (keys & KEY_A) {// A key (x on emulator) //FIXME: Als je teruggaat, crasht het
-        engine->setScene(new MainMenu(engine)); //Eventueel kunnen we hier een boodschap geven 'Are you sure you want to quit the level?' ofzo..
-    }
+
 
     if (zombies.empty()) {// All zombies dead? Start next wave
         nextWave();
@@ -139,6 +143,8 @@ void Level::tick(u16 keys) {
 
     updateMinions();
     updateZombies();
+
+
 }
 
 void Level::Scroll(bool toZombies) {
