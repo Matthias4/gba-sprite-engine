85095ee53d39ff7351c33df08cc69f845577f352
Anouk Michiels <anouk.michiels@student.uhasselt.be>
2019-12-05 11:31:34 +0100

    Added constructor of Basic zombie.

--- MinionsVsZombies/include/Enemies/Basic.h
+++ MinionsVsZombies/include/Enemies/Basic.h
@@ -5,4 +5,13 @@
 #ifndef GBA_SPRITE_ENGINE_PROJECT_BASIC_H
 #define GBA_SPRITE_ENGINE_PROJECT_BASIC_H
 
+#include "Zombie.h"
+
+class Basic: public Zombie {
+private:
+
+public:
+    Basic(int row, std::unique_ptr<Sprite> image);
+};
+
 #endif //GBA_SPRITE_ENGINE_PROJECT_BASIC_H
